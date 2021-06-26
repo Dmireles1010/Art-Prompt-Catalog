9de721ccfe91378c77e40eb2848dfdc9597fd951
David Mireles <dmireles1010@gmail.com>
2021-06-26 15:00:01 -0700

    v1.0.2

--- src/Component/NavbarHeader.js
+++ src/Component/NavbarHeader.js
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import {Navbar, Nav,OverlayTrigger,Popover} from 'react-bootstrap';
 
-
+import smile from "../images/smile.png"
 const popover = (
     <Popover id="popover-basic" data-trigger="focus" >
       <Popover.Title as="h3">About</Popover.Title>
@@ -10,13 +10,14 @@ const popover = (
         <hr/>
         Any <strong>NSFW</strong> art is blurred out. Clicking image will unblur and expand. <br/>
         <hr/>
-        Visit <i>Gatorbooru</i> for more art of the community.<br/>
+        Visit <a href={"https://goldengator.booru.org/index.php?page=post&s=list&tags=all"} target="_blank" rel="noopener noreferrer" >Gatorbooru</a> for more art of the community.<br/>
         <hr/>
-        Website built by <strong>Davu#8785</strong>. <br/>Message me on discord for any issues or concerns. 
+        Built by <strong>Davu#8785</strong> <img src={smile} alt="" width="24" height="18"/>  <br/>Message on Discord for any concerns. <br/>
+        <hr/>
+        Not affiliated with <a href={"https://www.twitch.tv/roflgator"} target="_blank" rel="noopener noreferrer">Roflgator</a>
       </Popover.Content>
     </Popover>
   );
-
 
 class NavbarHeader extends Component{
     render(){
@@ -28,7 +29,6 @@ class NavbarHeader extends Component{
                     <Nav className="mr-auto">
                         <Nav.Link  href={"https://discord.gg/RUGUd7H"} target="_blank" rel="noopener noreferrer">Discord</Nav.Link>
                         <Nav.Link  href={"https://www.reddit.com/r/thegoldengator/"} target="_blank" rel="noopener noreferrer">Reddit</Nav.Link>
-                        <Nav.Link  href={"https://www.twitch.tv/roflgator"} target="_blank" rel="noopener noreferrer">Rolfgator's Twitch</Nav.Link>
                         <Nav.Link  href={"https://goldengator.booru.org/index.php?page=post&s=list&tags=all"} target="_blank" rel="noopener noreferrer">Gatorbooru</Nav.Link>
                         <Nav.Link href={"https://vrchat-legends.fandom.com/wiki/The_Golden_Gator"} target="_blank" rel="noopener noreferrer" >Wiki</Nav.Link>
                         <OverlayTrigger trigger="click" placement="auto" rootClose overlay={popover}>
